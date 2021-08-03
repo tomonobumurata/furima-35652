882e923135cf5fd32ab97970ce475de7bc55877b
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-08-03 18:42:21 +0900

    ユーザーモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  # 英数字混合6文字以上の正規表現
+  VALID_PASSWORD_REGIX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/
+
+  validates :password,        presence: true, format: {with: VALID_PASSWORD_REGIX}
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name,       presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name,      presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :last_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/} 
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,        presence: true
 end

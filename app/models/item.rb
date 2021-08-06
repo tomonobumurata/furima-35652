58c9a165b66cbd59e503b0a2e1298914647d02c6
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-08-07 00:23:45 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  validates :title, :instruction, :image, :price, presence: true
+  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 }
 end

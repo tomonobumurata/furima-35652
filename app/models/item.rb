7f607709dcfea7c2475746315d6f56422705b064
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-08-07 01:05:31 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,22 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
-  validates :title, :instruction, :image, :price, presence: true
-  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 }
+
+  with_options presence: true do
+    validates :title 
+    validates :instruction
+    validates :image 
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id 
+      validates :shipping_fee_id
+      validates :prefecture_id
+      validates :days_to_ship_id 
+    end
+
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+    end
+  end
 end

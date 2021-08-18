bd27a7d7b9131d150aac22e634cc7a3a45fc5ac7
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-08-18 21:44:03 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_item, only: [:show, :edit, :update, :destroy]
-  before_action :login_judge, only: [:edit, :destroy]
+  before_action :login_judge, only: [:edit, :destroy, :update]
 
   def index
     @items = Item.all.order('created_at DESC')

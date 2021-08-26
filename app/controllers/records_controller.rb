319b301fc2e30ec4853fbd0ca3af78a6b26981b3
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-08-26 16:40:58 +0900

    フォームの情報を保存する機能

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,31 @@
 class RecordsController < ApplicationController
   def index
+    @record_address = RecordAddress.new
   end
 
   def create
+    @record_address = RecordAddress.new(record_params)
+    if @record_address.valid?
+      @record_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record_address).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :house_number,
+      :building_name,
+      :phone_number,
+    ).merge(
+      item_id: params[:item_id],
+      user_id: current_user.id
+    )
   end
 end

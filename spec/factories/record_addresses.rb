b76f58392527fbdca1064ee67a05d6b86e505998
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-08-28 18:06:28 +0900

    テストコードの記述

---
+++ spec/factories/record_addresses.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :record_address do
+    postal_code   { '123-4567'}
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city          { Faker::Address.city }
+    house_number  { '1-1-1'}
+    building_name { Faker::Address.community }
+    phone_number  { Faker::Number.number(digits: 11) }
+
+    association :user
+    association :item
+  end
+end
